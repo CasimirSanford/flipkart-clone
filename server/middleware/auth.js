1b5229054e4e077ead0f6518a9c34a5d9faf59a4
CasimirSanford <csanfo14@asu.edu>
2025-05-31 18:10:09 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,28 +1,33 @@
+
 const jwt = require('jsonwebtoken');
 const ErrorHandler = require('../utils/errorhandler');
 const aysnchandler = require('./aysnchandler');
-const UserModel = require('../models/usermodel');
+const UserModel = require('../models/usermodel')
 
-exports.isAuthenticatedUser = aysnchandler(async (req, res, next) => {
-    const { token } = req.cookies;
 
-    if (!token) {
-        return next(new ErrorHandler('Please Login First before Moving further', 400));
+exports.Authorization = aysnchandler(async(req,res,next)=>{
+    const {token} = req.cookies
+
+
+    if(!token){
+        return next(new ErrorHandler('Please Login First before Moving further', 400))
     }
 
-    const decode = jwt.verify(token, process.env.JWT);
+    const decode =  jwt.verify(token , process.env.JWT)
 
-    req.user = await UserModel.findById(decode.id);
+    req.user = await UserModel.findById(decode.id)
 
-    next();
-});
+    next()
 
-exports.authorizerole = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            return next(new ErrorHandler('You are not Allowed to Access This Route', 300));
-        }
-        next();
-    };
-};
+})
 
+exports.authorizerole = (...roles)=>{
+   return (req,res,next)=>{
+    if(!roles.includes(req.user.role)){
+        return next( new ErrorHandler('You are not Allowed to Access This Route', 300))
+       }   
+   next()
+   }
+   
+ 
+}
