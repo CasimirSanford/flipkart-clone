5ed820e30ea567b07142d6c291b57db7daf46d27
CasimirSanford <csanfo14@asu.edu>
2025-05-31 17:54:52 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,33 +1,34 @@
-
 const jwt = require('jsonwebtoken');
 const ErrorHandler = require('../utils/errorhandler');
 const aysnchandler = require('./aysnchandler');
-const UserModel = require('../models/usermodel')
+const UserModel = require('../models/usermodel');
 
+exports.isAuthenticatedUser = aysnchandler(async (req, res, next) => {
+    const { token } = req.cookies;
 
-exports.Authorization = aysnchandler(async(req,res,next)=>{
-    const {token} = req.cookies
-
-
-    if(!token){
-        return next(new ErrorHandler('Please Login First before Moving further', 400))
+    if (!token) {
+        return next(new ErrorHandler('Please login first before moving further', 400));
     }
 
-    const decode =  jwt.verify(token , process.env.JWT)
+    try {
+        const decoded = jwt.verify(token, process.env.JWT);
+        req.user = await UserModel.findById(decoded.id);
 
-    req.user = await UserModel.findById(decode.id)
+        if (!req.user) {
+            return next(new ErrorHandler('User not found from token', 404));
+        }
 
-    next()
+        next();
+    } catch (err) {
+        return next(new ErrorHandler('Invalid or expired token', 401));
+    }
+});
 
-})
-
-exports.authorizerole = (...roles)=>{
-   return (req,res,next)=>{
-    if(!roles.includes(req.user.role)){
-        return next( new ErrorHandler('You are not Allowed to Access This Route', 300))
-       }   
-   next()
-   }
-   
- 
-}
+exports.authorizerole = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler('You are not allowed to access this route', 403));
+        }
+        next();
+    };
+};
